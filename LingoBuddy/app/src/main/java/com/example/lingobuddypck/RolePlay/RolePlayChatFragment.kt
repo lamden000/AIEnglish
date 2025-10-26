13e296c5d0e376d2fa0437f308cf59a592b88391
mainguyenuit <22520991@gm.uit.edu.vn>
2025-10-26 18:02:12 +0700

    fix: Add default fragment

---
+++ LingoBuddy/app/src/main/java/com/example/lingobuddypck/RolePlay/RolePlayChatFragment.kt
@@ -0,0 +1,10 @@
+package com.example.lingobuddypck.RolePlay
+
+import android.speech.tts.TextToSpeech
+import androidx.fragment.app.Fragment
+
+class RolePlayChatFragment : Fragment(), TextToSpeech.OnInitListener {
+    override fun onInit(status: Int) {
+        TODO("Not yet implemented")
+    }
+}
